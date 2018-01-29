abfeaced587f89a830624987d688e18685ee80b4
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-01-29 14:50:22 -0600

    Attr. Locking 4 all Conn Sim Dataset Added

--- analysisScripts/analysisTypes.py
+++ analysisScripts/analysisTypes.py
@@ -5,6 +5,8 @@ class AnalysisTypes:
     def __init__(self, simlog):
         self.log = simlog
 
+
+
     def getTotalWaitingTimeOfThisUser(self, userIndex):
         total_waiting_time = 0
         myLogParser = ParsingUtility()
@@ -22,6 +24,9 @@ class AnalysisTypes:
 
         return -1
 
+
+
+
     def getAverageWaitTime(self, num_of_collabs):
         totalWaitTime = 0
         for aCollab in range(0, num_of_collabs):
@@ -31,3 +36,50 @@ class AnalysisTypes:
         return totalWaitTime / num_of_collabs
 
 
+
+
+
+    def getCollaborativeWorkflowDesignTime(self):
+        startLog = self.log[0] #the first log while designing this workflow
+        endLog = self.log[len(self.log)-1] #the end log while designing this workflow
+
+        myLogParser = ParsingUtility()
+        startLogTime = myLogParser.getTimeStamp(startLog)
+        endLogTime  = myLogParser.getTimeStamp(endLog)
+
+        myLogTimeUtil = TimeUtility()
+
+        return myLogTimeUtil.getTimeDifference(startLogTime, endLogTime)
+
+
+
+
+
+    def getTotalNumberOfUpdates(self):
+        numOfUpdates = 0
+        myLogParser = ParsingUtility()
+
+        for i in range(0, len(self.log)):
+            userActivity, collabID = myLogParser.getUserActivityTypeAndCollabID(self.log[i])
+            if userActivity == 'UPDATE':
+                numOfUpdates += 1
+
+        return numOfUpdates
+
+
+
+
+
+    def getTotalUpdatesPerUnitTime(self, perUnitTime):
+        updatePerMilliSec  = self.getTotalNumberOfUpdates() / self.getCollaborativeWorkflowDesignTime()
+
+        return updatePerMilliSec*perUnitTime
+
+
+
+    #TODO: fix the calculation from Confucius paper
+    def getWorkflowCompositionEfficiency(self, numOfCollabs, numOfTasks):
+        totalUpdates = self.getTotalNumberOfUpdates()
+        totalCompositionTime = self.getCollaborativeWorkflowDesignTime()
+
+        return  totalUpdates*100/((totalCompositionTime/60000)*numOfTasks*numOfCollabs)
--- analysisScripts/main.py
+++ analysisScripts/main.py
@@ -9,6 +9,14 @@ fileUtility = FileUtility()
 
 
 
+
+
+
+
+
+print("\n\n")
+print("===============================================")
+print("AVG. WAITING TIME PER COLLAB")
 for numOfCollab in range(1, 20):
     simLog = fileUtility.loadSimulationLog('../simulation_datasets/TurnBased/collab_'+ str(numOfCollab) +'.log')
     analysisTypes = AnalysisTypes(simLog[1:len(simLog):])
@@ -17,3 +25,33 @@ for numOfCollab in range(1, 20):
 
 
 
+print("\n\n")
+print("===============================================")
+print("TOTAL WORKFLOW DESIGN TIME")
+for numOfCollab in range(1, 20):
+    simLog = fileUtility.loadSimulationLog('../simulation_datasets/TurnBased/collab_'+ str(numOfCollab) +'.log')
+    analysisTypes = AnalysisTypes(simLog[1:len(simLog):])
+    print(analysisTypes.getCollaborativeWorkflowDesignTime())
+
+
+
+
+
+
+print("\n\n")
+print("===============================================")
+print("AVG. UPDATES PER MIN")
+for numOfCollab in range(1, 20):
+    simLog = fileUtility.loadSimulationLog('../simulation_datasets/TurnBased/collab_'+ str(numOfCollab) +'.log')
+    analysisTypes = AnalysisTypes(simLog[1:len(simLog):])
+    print(analysisTypes.getTotalUpdatesPerUnitTime(60000))
+
+
+
+print("\n\n")
+print("===============================================")
+print("EFFICEINCY")
+for numOfCollab in range(1, 20):
+    simLog = fileUtility.loadSimulationLog('../simulation_datasets/TurnBased/collab_'+ str(numOfCollab) +'.log')
+    analysisTypes = AnalysisTypes(simLog[1:len(simLog):])
+    print(analysisTypes.getWorkflowCompositionEfficiency(numOfCollab, 25))
